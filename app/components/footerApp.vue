565c69ce295334520c26959e507b7cf40249f78a
Dmong04 <168316345+Dmong04@users.noreply.github.com>
2026-04-09 15:20:52 -0600

    Ajustes de header y footer

--- app/components/footerApp.vue
+++ app/components/footerApp.vue
@@ -2,10 +2,47 @@
 
 </script>
 <template>
-    <div>
-        <footer
-            class="bg-gray-100 py-4 text-center text-sm text-gray-600 dark:bg-gray-800 dark:text-gray-400 transition-all duration-300">
-            &copy; 2026 SIPAUNA. Todos los derechos reservados.
-        </footer>
-    </div>
+    <footer class="px-6 md:px-16 lg:px-24 xl:px-32 pt-8 w-full text-gray-500">
+        <div class="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500/30 pb-6">
+            <div class="md:max-w-96">
+                <div class="flex items-center">
+                    <img src="/img/LogoUNA.png" alt="Logo" class="h-32 w-40">
+                    <div class="flex flex-col">
+                        <span class="dark:text-white text-4xl">SIPAUNA</span>
+                        <span class="dark:text-white">Sistema para Préstamo de Aulas de la Universidad Nacional</span>
+                    </div>
+                </div>
+                <p class="mt-6 text-sm dark:text-white">
+                    Nacido de un proyecto universitario, al servicio de la institución.
+                </p>
+            </div>
+            <div class="flex-1 flex items-start md:justify-end gap-20">
+                <div>
+                    <h2 class="font-semibold mb-5 dark:text-white text-gray-800">Información</h2>
+                    <ul class="dark:text-white text-sm space-y-2">
+                        <li><a href="#">Acerca de SIPAUNA</a></li>
+                        <li><a href="#">Desarrolladores</a></li>
+                        <li><a href="#">Contacto</a></li>
+                        <li><a href="#">Misión y visión</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h2 class="font-semibold dark:text-white mb-5 text-gray-800">¿Te interesa saber más?</h2>
+                    <div class="text-sm space-y-2 dark:text-white">
+                        <p>Envíanos tu información y te contactaremos para contarte más acerca de SIPAUNA, cómo funciona
+                            y si gustarías apoyarnos.</p>
+                        <div class="flex items-center gap-2 pt-4">
+                            <input
+                                class="border dark:border-white border-gray-500/30 dark:placeholder-white placeholder-gray-500 focus:ring-2 ring-red-500 outline-none w-full max-w-64 h-9 rounded px-2"
+                                type="email" placeholder="Digita tu correo institucional">
+                            <button class="bg-red-500 w-24 h-9 text-white rounded">Enviar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <p class="pt-4 text-center text-sm dark:text-white md:text-sm pb-5">
+            Copyright 2026<a href="https://prebuiltui.com"> &copy;SIPAUNA</a>. Todos los derechos reservados.
+        </p>
+    </footer>
 </template>
--- app/components/headerApp.vue
+++ app/components/headerApp.vue
@@ -14,36 +14,44 @@ const toggleTheme = () => {
 </script>
 
 <template>
-  <header
-    class="bg-white py-2  dark:bg-gray-900  transition-all duration-300">
-
-
-    <div class="flex items-center justify-between max-w-7xl mx-auto px-4">
-
-
+  <header class="sticky top-0 z-50 bg-white py-2  dark:bg-gray-900  transition-all duration-300">
+    <div class="flex items-center justify-between md:justify-around max-w-7xl mx-auto px-4">
       <img src="/img/LogoUNA.png" alt="SIPAUNA Logo" class="h-15 w-auto" />
-
-
-      <nav class="hidden md:flex gap-6 text-sm items-center">
-        <NuxtLink to="/" class="text-gray-800 dark:text-white hover:text-red-400 transition-colors duration-200">Acerca
-          de SIPAUNA</NuxtLink>
-        <NuxtLink to="/" class="text-gray-800 dark:text-white hover:text-red-400 transition-colors duration-200">Acerca
-          de la U</NuxtLink>
-        <NuxtLink to="/" class="text-gray-800 dark:text-white hover:text-red-400 transition-colors duration-200">
-          Consultas</NuxtLink>
-        <button @click="toggleTheme"
-          class="text-white dark:text- flex items-center gap-2 bg-blue-700 dark:bg-gray-800 rounded-lg px-3 py-2 hover:bg-blue-500 dark:hover:bg-gray-700 duration-300 transition-colors ease-in">
-          <svg v-if="colorMode.value !== 'dark'" class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg"
-            fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M12 3v2m0 14v2m9-9h-2M5 12H3m15.364-6.364l-1.414 1.414M7.05 16.95l-1.414 1.414M16.95 16.95l1.414 1.414M7.05 7.05L5.636 5.636M12 7a5 5 0 100 10 5 5 0 000-10z" />
-          </svg>
-          <svg v-else class="w-6 h-6 text-white fill-current" viewBox="0 0 20 20">
-            <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-          </svg>
-          <span class="text-slate-200">Cambiar tema</span>
-        </button>
+      <nav class="hidden md:flex text-sm items-center">
+        <div class="flex items-center justify-center gap-1">
+          <div class="gap-1">
+            <NuxtLink to="/dashboard"
+              class="border-b-2 border-transparent hover:border-red-500 hover:text-red-500 dark:text-white text-gray-600 text-sm font-medium inline-flex items-center px-2 h-full transition-colors duration-200">
+              Inicio
+            </NuxtLink>
+            <NuxtLink to="/dashboard/schedules"
+              class="border-b-2 border-transparent hover:border-red-500 hover:text-red-500 dark:text-white text-gray-600 text-sm font-medium inline-flex items-center px-2 h-full transition-colors duration-200">
+              Horarios
+            </NuxtLink>
+            <NuxtLink to="/dashboard/booking"
+              class="border-b-2 border-transparent hover:border-red-500 hover:text-red-500 dark:text-white text-gray-600 text-sm font-medium inline-flex items-center px-2 h-full transition-colors duration-200">
+              Solicitud de reservación
+            </NuxtLink>
+            <NuxtLink to="/dashboard/availability"
+              class="border-b-2 border-transparent hover:border-red-500 hover:text-red-500 dark:text-white text-gray-600 text-sm font-medium inline-flex items-center px-2 h-full transition-colors duration-200">
+              Comprobar disponibilidad
+            </NuxtLink>
+          </div>
+        </div>
       </nav>
+
+      <button @click="toggleTheme"
+        class="hidden text-white md:flex items-center gap-2 bg-blue-700 dark:bg-gray-800 rounded-lg px-3 py-2 hover:bg-blue-500 dark:hover:bg-gray-700 duration-300 transition-colors ease-in">
+        <svg v-if="colorMode.value !== 'dark'" class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
+          viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M12 3v2m0 14v2m9-9h-2M5 12H3m15.364-6.364l-1.414 1.414M7.05 16.95l-1.414 1.414M16.95 16.95l1.414 1.414M7.05 7.05L5.636 5.636M12 7a5 5 0 100 10 5 5 0 000-10z" />
+        </svg>
+        <svg v-else class="w-6 h-6 text-white fill-current" viewBox="0 0 20 20">
+          <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+        </svg>
+        <span class="text-slate-200">Cambiar tema</span>
+      </button>
 
       <!-- Botón hamburguesa -->
       <button @click="toggleMenu" class="md:hidden text-gray-800 dark:text-white">
@@ -59,12 +67,26 @@ const toggleTheme = () => {
     <!-- Nav Móvil (fuera del flex) -->
     <div v-if="menuOpen"
       class="md:hidden flex flex-col px-6 pt-3 pb-4 gap-3 border-t border-gray-200 dark:border-white/10 mt-2">
-      <NuxtLink to="/" class="text-gray-800 dark:text-white hover:text-red-400 transition-colors duration-200">Acerca
-        de SIPAUNA</NuxtLink>
-      <NuxtLink to="/" class="text-gray-800 dark:text-white hover:text-red-400 transition-colors duration-200">Acerca
-        de la U</NuxtLink>
-      <NuxtLink to="/" class="text-gray-800 dark:text-white hover:text-red-400 transition-colors duration-200">
-        Consultas</NuxtLink>
+      <div>
+        <div class="flex flex-col gap-4">
+          <NuxtLink to="/dashboard"
+            class="border-b-2 border-transparent hover:border-red-500 hover:text-red-500 dark:text-white text-gray-600 text-sm font-medium inline-flex items-center px-2 h-full transition-colors duration-200">
+            Inicio
+          </NuxtLink>
+          <NuxtLink to="/dashboard/schedules"
+            class="border-b-2 border-transparent hover:border-red-500 hover:text-red-500 dark:text-white text-gray-600 text-sm font-medium inline-flex items-center px-2 h-full transition-colors duration-200">
+            Horarios
+          </NuxtLink>
+          <NuxtLink to="/dashboard/booking"
+            class="border-b-2 border-transparent hover:border-red-500 hover:text-red-500 dark:text-white text-gray-600 text-sm font-medium inline-flex items-center px-2 h-full transition-colors duration-200">
+            Solicitud de reservación
+          </NuxtLink>
+          <NuxtLink to="/dashboard/availability"
+            class="border-b-2 border-transparent hover:border-red-500 hover:text-red-500 dark:text-white text-gray-600 text-sm font-medium inline-flex items-center px-2 h-full transition-colors duration-200">
+            Comprobar disponibilidad
+          </NuxtLink>
+        </div>
+      </div>
       <button @click="toggleTheme"
         class="text-white dark:text- flex items-center gap-2 bg-blue-700 dark:bg-gray-800 rounded-lg px-3 py-2 hover:bg-blue-500 dark:hover:bg-gray-700 duration-300 transition-colors ease-in">
         <svg v-if="colorMode.value !== 'dark'" class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
